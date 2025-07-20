843a80ddedd565ce80961f28c3d905ccb4af4698
Diasiris123 <ilia.tyupa@gmail.com>
2025-07-20 22:23:14 +0300

    yml

---
+++ QATiltanHomework/Assets/_Main/Task2/Scripts/Tests/PlayModeTests/TorchPerformanceTest.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Diagnostics;
+
+public class TorchPerformanceTest
+{
+    [Test]
+    public void Torch_LightUp_Performance()
+    {
+        var stopwatch = new Stopwatch();
+        var torches = new Torch[1000];
+
+        for (int i = 0; i < torches.Length; i++)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var torch = go.AddComponent<Torch>();
+            torch.rend = go.GetComponent<Renderer>();
+            if (torch.rend.sharedMaterial == null)
+                torch.rend.sharedMaterial = new Material(Shader.Find("Standard"));
+            torches[i] = torch;
+        }
+
+        stopwatch.Start();
+        foreach (var torch in torches)
+        {
+            torch.LightUp();
+        }
+        stopwatch.Stop();
+
+        UnityEngine.Debug.Log($"Lighting 1000 torches took {stopwatch.ElapsedMilliseconds}ms");
+
+        Assert.Less(stopwatch.ElapsedMilliseconds, 200, "Performance: Lighting 1000 torches took too long");
+    }
+}
